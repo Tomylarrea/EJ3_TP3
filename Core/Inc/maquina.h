f10e0ac9efa2da967e25ef63f468431839135e47
Tomylarrea <tomylarrea512@gmail.com>
2026-04-27 19:23:58 -0300

    Initial commit

---
+++ Core/Inc/maquina.h
@@ -0,0 +1,35 @@
+/*
+ * maquina.h
+ *
+ *  Created on: 27 abr 2026
+ *      Author: tomyl
+ */
+
+#ifndef INC_MAQUINA_H_
+#define INC_MAQUINA_H_
+
+
+#include <stdint.h>
+#include "main.h"
+
+typedef enum {
+    ESTADO_SELECCION,
+    ESTADO_EXPENDIENDO
+} Estado_maquina;
+
+typedef enum {
+    EVENTO_BOTON_SELEC,
+    EVENTO_FIN_EXP,
+    EVENTO_BOTON_ROTAR
+} Evento_maquina;
+
+extern uint8_t producto[4];
+extern uint8_t indice;
+extern uint8_t flag_start;
+
+
+Estado_maquina iniciar_maquina(void);
+Estado_maquina MAQ_procesar_evento(Estado_maquina actual, Evento_maquina evento);
+
+
+#endif /* INC_MAQUINA_H_ */
---
+++ Core/Src/maquina.c
@@ -0,0 +1,78 @@
+/*
+ * maquina.c
+ *
+ *  Created on: 27 abr 2026
+ *      Author: tomyl
+ */
+
+
+
+#include <stdint.h>
+#include "main.h"
+#include "maquina.h"
+#include "stm32f1xx_it.h"
+
+uint8_t producto[4] = {1, 2, 3, 4};
+uint8_t cantidad = sizeof(producto) / sizeof(producto[0]);
+uint8_t indice = 1;
+uint8_t flag_start = 0;
+
+void rotacion_productos(){
+	if (indice < cantidad){
+		indice++;
+	} else { indice = 1; }
+}
+
+void habilitar_mecanismo(){
+	 flag_start = 1;
+}
+
+
+/* Process evento and execute actions */
+Estado_maquina MAQ_procesar_evento(Estado_maquina actual, Evento_maquina evento) {
+    Estado_maquina siguiente = actual;
+    switch (actual) {
+        case ESTADO_SELECCION:
+            if (evento == EVENTO_BOTON_SELEC) {
+                siguiente = ESTADO_EXPENDIENDO;
+                break;
+            }
+            if (evento == EVENTO_BOTON_ROTAR) {
+                rotacion_productos();
+                siguiente = ESTADO_SELECCION;
+                break;
+            }
+            break;
+        case ESTADO_EXPENDIENDO:
+            if (evento == EVENTO_FIN_EXP) {
+                siguiente = ESTADO_SELECCION;
+                break;
+            }
+            break;
+        default: break;
+    }
+
+    if (siguiente != actual) {
+        switch (siguiente) {
+            case ESTADO_EXPENDIENDO:
+                habilitar_mecanismo();
+                break;
+            default: break;
+        }
+    }
+    return siguiente;
+}
+
+/* Initialize FSM */
+Estado_maquina iniciar_maquina(void) {
+    Estado_maquina inicial = ESTADO_SELECCION;
+    rotacion_productos();
+    return inicial;
+}/*
+ * maquina.c
+ *
+ *  Created on: 27 abr 2026
+ *      Author: tomyl
+ */
+
+
